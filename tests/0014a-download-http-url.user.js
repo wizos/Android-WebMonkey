f0b93a370cfed859786e3baf1e7d09f05c6d9bff
Warren R. Bank <warren.r.bank@gmail.com>
2024-11-17 01:51:13 -0800

    add tests: 'GM_download'

---
+++ tests/0014a-download-http-url.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         test: GM_download (network URL)
+// @namespace    WebViewWM
+// @match        *://*
+// @run-at       document-end
+// ==/UserScript==
+
+var run_download_test = function() {
+  var url  = 'https://github.com/warren-bank/Android-WebMonkey/raw/v04.02.03/android-studio-project/WebMonkey/src/main/res/drawable/launcher.png'
+  var name = 'WebMonkey.png'
+
+  unsafeWindow.alert('downloading: ' + url.substring(0, 100))
+
+  GM_download(url, name)
+}
+
+run_download_test()
---
+++ tests/0014b-download-data-uri.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         test: GM_download (base64-encoded data: URI)
+// @namespace    WebViewWM
+// @match        *://*
+// @run-at       document-end
+// ==/UserScript==
+
+var run_download_test = function() {
+  var response = GM_xmlhttpRequest({
+    'method':       'GET',
+    'url':          'https://github.com/warren-bank/Android-WebMonkey/raw/v04.02.03/android-studio-project/WebMonkey/src/main/res/drawable/launcher.png',
+    'responseType': 'text_datauri',
+    'synchronous':  true
+  })
+
+  var url  = response.response
+  var name = 'WebMonkey.png'
+
+  unsafeWindow.alert('downloading: ' + url.substring(0, 100))
+
+  GM_download(url, name)
+}
+
+run_download_test()
