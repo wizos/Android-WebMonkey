36136db45b0cc8d5aa9fce934dbeb4a9f398a8e9
Warren R. Bank <warren.r.bank@gmail.com>
2024-11-27 17:33:00 -0800

    refactor the way the JS GM API library is composed
    Rather than using a StringBuilder and assembling substrings,
    migrate the aggregate to a template file,
    and use substitution to interpolate dynamic variables.

--- android-studio-project/libs/webview-gm-lib/src/main/java/at/pardus/android/webview/gm/util/ScriptJsCode.java
+++ android-studio-project/libs/webview-gm-lib/src/main/java/at/pardus/android/webview/gm/util/ScriptJsCode.java
@@ -6,6 +6,8 @@ import at.pardus.android.webview.gm.model.ScriptRequire;
 import at.pardus.android.webview.gm.util.ResourceHelper;
 import at.pardus.android.webview.gm.util.ScriptInfo;
 
+import org.apache.commons.lang3.StringUtils;
+
 import android.content.Context;
 import android.os.Build;
 import android.text.TextUtils;
@@ -17,9 +19,10 @@ public class ScriptJsCode {
 
   private static final String GLOBAL_JS_OBJECT = "unsafeWindow.wrappedJSObject";
 
-  private static String GM_API_LEGACY_MISSING = "";
-  private static String GM_API_LEGACY         = "";
-  private static String GM_API_V4_POLYFILL    = "";
+  private static String GM_API_LEGACY_MISSING         = "";
+  private static String GM_API_LEGACY                 = "";
+  private static String GM_API_LEGACY_SIGNED_TEMPLATE = "";
+  private static String GM_API_V4_POLYFILL            = "";
 
   private static String JS_CLOSURE_1 = "";  // from: start of closure.     => to: start of GM API.
   private static String JS_CLOSURE_2 = "";  // from: end of GM API.        => to: start of userscript.
@@ -36,6 +39,12 @@ public class ScriptJsCode {
     if (TextUtils.isEmpty(GM_API_LEGACY)) {
       try {
         GM_API_LEGACY = ResourceHelper.getRawStringResource(context, R.raw.gm_api_legacy);
+      }
+      catch(Exception e) {}
+    }
+    if (TextUtils.isEmpty(GM_API_LEGACY_SIGNED_TEMPLATE)) {
+      try {
+        GM_API_LEGACY_SIGNED_TEMPLATE = ResourceHelper.getRawStringResource(context, R.raw.gm_api_legacy_signed_template);
       }
       catch(Exception e) {}
     }
@@ -113,6 +122,35 @@ public class ScriptJsCode {
   protected String getJsApi(Script script, String jsBridgeName, String secret) {
     StringBuilder sb;
 
+    // jsApi
+    sb = new StringBuilder(4 * 1024);
+    sb.append(GLOBAL_JS_OBJECT + " = " + GLOBAL_JS_OBJECT + " || {};\n");
+    sb.append(GM_API_LEGACY_MISSING);
+    sb.append(GM_API_LEGACY);
+
+    sb.append(GLOBAL_JS_OBJECT + "._GM_formatXmlHttpResponse = _GM_formatXmlHttpResponse;\n");
+
+    // -------------------------
+    // Greasemonkey API (legacy)
+    // -------------------------
+
+    sb.append("var GM_info = ");
+    sb.append(  ScriptInfo.toJSONString(script));
+    sb.append(";");
+    sb.append("\n");
+
+    sb.append(
+      interpolateSignedTemplate(script, jsBridgeName, secret)
+    );
+
+    sb.append(GM_API_V4_POLYFILL);
+
+    return sb.toString();
+  }
+
+  private String interpolateSignedTemplate(Script script, String jsBridgeName, String secret) {
+    StringBuilder sb;
+
     // defaultSignature
     sb = new StringBuilder(1 * 1024);
     sb.append("\"");
@@ -134,283 +172,28 @@ public class ScriptJsCode {
     String callbackPrefix = sb.toString().replaceAll("[^0-9a-zA-Z_]", "");
     sb = null;
 
-    // jsApi
-    sb = new StringBuilder(4 * 1024);
-    sb.append(GLOBAL_JS_OBJECT + " = " + GLOBAL_JS_OBJECT + " || {};\n");
-    sb.append(GM_API_LEGACY_MISSING);
-    sb.append(GM_API_LEGACY);
-
-    sb.append(GLOBAL_JS_OBJECT + "._GM_formatXmlHttpResponse = _GM_formatXmlHttpResponse;\n");
-
-    // -------------------------
-    // Greasemonkey API (legacy)
-    // -------------------------
-
-    sb.append("var GM_info = ");
-    sb.append(  ScriptInfo.toJSONString(script));
-    sb.append(";");
-    sb.append("\n");
-
-    sb.append("var GM_listValues = function() {");
-    sb.append(  "return " + jsBridgeName + ".listValues(" + defaultSignature + ")");
-    sb.append(    ".split(',');");
-    sb.append("};");
-    sb.append("\n");
-
-    sb.append("var GM_getValue = function(name, defaultValue) {");
-    sb.append(  "if (defaultValue === undefined) {defaultValue = null;}");
-    sb.append(  "defaultValue = JSON.stringify(defaultValue);");
-    sb.append(  "return JSON.parse(");
-    sb.append(    jsBridgeName + ".getValue(" + defaultSignature + ", name, defaultValue)");
-    sb.append(  ");");
-    sb.append("};");
-    sb.append("\n");
-
-    sb.append("var GM_setValue = function(name, value) {");
-    sb.append(  "if (value === undefined) {value = null;}");
-    sb.append(  "value = JSON.stringify(value);");
-    sb.append(  jsBridgeName + ".setValue(" + defaultSignature + ", name, value);");
-    sb.append("};");
-    sb.append("\n");
-
-    sb.append("var GM_deleteValue = function(name) {");
-    sb.append(  jsBridgeName + ".deleteValue(" + defaultSignature + ", name);");
-    sb.append("};");
-    sb.append("\n");
-
-    sb.append("var GM_log = function(message) {");
-    sb.append(  jsBridgeName + ".log(" + defaultSignature + ", message);");
-    sb.append("};");
-    sb.append("\n");
-
-    sb.append("var GM_getResourceURL = function(resourceName) {");
-    sb.append(  "return " + jsBridgeName + ".getResourceURL(" + defaultSignature + ", resourceName);");
-    sb.append("};");
-    sb.append("\n");
-
-    sb.append("var GM_getResourceText = function(resourceName) {");
-    sb.append(  "return " + jsBridgeName + ".getResourceText(" + defaultSignature + ", resourceName);");
-    sb.append("};");
-    sb.append("\n");
-
-    sb.append("var GM_xmlhttpRequest = function(details) {");
-    // short-circuit data: URI
-    sb.append(  "var dataUri, UUID, response, result;");
-    sb.append(  "dataUri = _GM_parseDataUri(details.url);");
-    // process data: URI
-    sb.append(  "if (dataUri) {");
-    sb.append(    "UUID = _GM_writeToCacheFile(dataUri.data);");
-    sb.append(    "if (UUID) {");
-    sb.append(      "response = {responseCacheUUID: UUID, mimeType: dataUri.mimeType, readyState: 4, status: 200, statusText: 'OK', lengthComputable: true, loaded: dataUri.byteLength, total: dataUri.byteLength};");
-    sb.append(    "}");
-    sb.append(  "}");
-    // process http: URL
-    sb.append(  "if (!response) {");
-    // onabort
-    sb.append(    "if (details.onabort) {");
-    sb.append(      GLOBAL_JS_OBJECT + "." + callbackPrefix + "GM_onAbortCallback = details.onabort;");
-    sb.append(      "details.onabort = '"  + callbackPrefix + "GM_onAbortCallback';");
-    sb.append(    "}");
-    // onerror
-    sb.append(    "if (details.onerror) {");
-    sb.append(      GLOBAL_JS_OBJECT + "." + callbackPrefix + "GM_onErrorCallback = details.onerror;");
-    sb.append(      "details.onerror = '"  + callbackPrefix + "GM_onErrorCallback';");
-    sb.append(    "}");
-    // onload
-    sb.append(    "if (details.onload) {");
-    sb.append(      GLOBAL_JS_OBJECT + "." + callbackPrefix + "GM_onLoadCallback = details.onload;");
-    sb.append(      "details.onload = '"   + callbackPrefix + "GM_onLoadCallback';");
-    sb.append(    "}");
-    // onprogress
-    sb.append(    "if (details.onprogress) {");
-    sb.append(      GLOBAL_JS_OBJECT + "."   + callbackPrefix + "GM_onProgressCallback = details.onprogress;");
-    sb.append(      "details.onprogress = '" + callbackPrefix + "GM_onProgressCallback';");
-    sb.append(    "}");
-    // onreadystatechange
-    sb.append(    "if (details.onreadystatechange) {");
-    sb.append(      GLOBAL_JS_OBJECT + "."           + callbackPrefix + "GM_onReadyStateChange = details.onreadystatechange;");
-    sb.append(      "details.onreadystatechange = '" + callbackPrefix + "GM_onReadyStateChange';");
-    sb.append(    "}");
-    // ontimeout
-    sb.append(    "if (details.ontimeout) {");
-    sb.append(      GLOBAL_JS_OBJECT + "."  + callbackPrefix + "GM_onTimeoutCallback = details.ontimeout;");
-    sb.append(      "details.ontimeout = '" + callbackPrefix + "GM_onTimeoutCallback';");
-    sb.append(    "}");
-    // upload
-    sb.append(    "if (details.upload) {");
-    // upload.onabort
-    sb.append(      "if (details.upload.onabort) {");
-    sb.append(        GLOBAL_JS_OBJECT + "."       + callbackPrefix + "GM_uploadOnAbortCallback = details.upload.onabort;");
-    sb.append(        "details.upload.onabort = '" + callbackPrefix + "GM_uploadOnAbortCallback';");
-    sb.append(      "}");
-    // upload.onerror
-    sb.append(      "if (details.upload.onerror) {");
-    sb.append(        GLOBAL_JS_OBJECT + "."       + callbackPrefix + "GM_uploadOnErrorCallback = details.upload.onerror;");
-    sb.append(        "details.upload.onerror = '" + callbackPrefix + "GM_uploadOnErrorCallback';");
-    sb.append(      "}");
-    // upload.onload
-    sb.append(      "if (details.upload.onload) {");
-    sb.append(        GLOBAL_JS_OBJECT + "."      + callbackPrefix + "GM_uploadOnLoadCallback = details.upload.onload;");
-    sb.append(        "details.upload.onload = '" + callbackPrefix + "GM_uploadOnLoadCallback';");
-    sb.append(      "}");
-    // upload.onprogress
-    sb.append(      "if (details.upload.onprogress) {");
-    sb.append(        GLOBAL_JS_OBJECT + "."          + callbackPrefix + "GM_uploadOnProgressCallback = details.upload.onprogress;");
-    sb.append(        "details.upload.onprogress = '" + callbackPrefix + "GM_uploadOnProgressCallback';");
-    sb.append(      "}");
-    // upload
-    sb.append(    "}");
-    // response value: WebViewXmlHttpResponse.toJSONString()
-    sb.append(    "response = JSON.parse(");
-    sb.append(      jsBridgeName + ".xmlHttpRequest(" + defaultSignature + ", JSON.stringify(details))");
-    sb.append(    ");");
-    sb.append(  "}");
-    // format response value
-    sb.append(  "result = _GM_formatXmlHttpResponse(details, response);");
-    // fire async events for data: URI
-    sb.append(  "if (dataUri) {");
-    sb.append(    "if (details.onprogress) {details.onprogress(result);}");
-    sb.append(    "if (details.onload)     {details.onload(result);}");
-    sb.append(  "}");
-    // return sync formatted response value
-    sb.append(  "return result;");
-    sb.append("};");
-    sb.append("\n");
-
-    sb.append("var _GM_writeToCacheFile = function(data) {");
-    sb.append(  "var bytes_per_chunk = 1050; /* multiple of 3 */");
-    sb.append(  "var index_start, index_end, UUID, OK, chunk_size, chunk_base64;");
-    sb.append(  "index_start = 0;");
-    sb.append(  "index_end = 0;");
-    sb.append(  "UUID = String(Date.now());");
-    sb.append(  "OK = true;");
-    sb.append(  "if (typeof data === 'string') { /* base64 */");
-    sb.append(    "chunk_size = _GM_bytesToBase64Length(bytes_per_chunk);");
-    sb.append(    "while (OK && (index_start < data.length)) {");
-    sb.append(      "index_end = (index_start + chunk_size);");
-    sb.append(      "if (index_end > data.length) index_end = data.length;");
-    sb.append(      "chunk_base64 = data.substring(index_start, index_end);");
-    sb.append(      "OK = " + jsBridgeName + ".writeToCacheFile(" + defaultSignature + ", UUID, chunk_base64);");
-    sb.append(      "index_start = index_end;");
-    sb.append(    "}");
-    sb.append(  "}");
-    sb.append(  "else if ((data instanceof ArrayBuffer) || (data instanceof Uint8Array)) {");
-    sb.append(    "if (data instanceof ArrayBuffer) {");
-    sb.append(      "data = new Uint8Array(data);");
-    sb.append(    "}");
-    sb.append(    "chunk_size = bytes_per_chunk;");
-    sb.append(    "while (OK && (index_start < data.byteLength)) {");
-    sb.append(      "index_end = (index_start + chunk_size);");
-    sb.append(      "if (index_end > data.byteLength) index_end = data.byteLength;");
-    sb.append(      "chunk_base64 = btoa(String.fromCharCode.apply(null, data.slice(index_start, index_end)));");
-    sb.append(      "OK = " + jsBridgeName + ".writeToCacheFile(" + defaultSignature + ", UUID, chunk_base64);");
-    sb.append(      "index_start = index_end;");
-    sb.append(    "}");
-    sb.append(  "}");
-    sb.append(  "return (index_end && OK) ? UUID : null;");
-    sb.append("}");
-    sb.append("\n");
-
-    sb.append("var _GM_readFromCacheFile = function(UUID, totalBytes) {");
-    sb.append(  "var arrayBuffer = uint8Buffer = null;");
-    sb.append(  "var base64Chunk = uint8Chunk = null;");
-    sb.append(  "var byteOffset = 0;");
-    sb.append(  "try {");
-    sb.append(    "arrayBuffer = new ArrayBuffer(totalBytes);");
-    sb.append(    "uint8Buffer = new Uint8Array(arrayBuffer);");
-    sb.append(    "while(base64Chunk = " + jsBridgeName + ".readFromCacheFile(" + defaultSignature + ", UUID, byteOffset)) {");
-    sb.append(      "uint8Chunk = new Uint8Array(");
-    sb.append(        "_GM_base64ToArrayBuffer(base64Chunk)");
-    sb.append(      ");");
-    sb.append(      "uint8Buffer.set(uint8Chunk, byteOffset);");
-    sb.append(      "byteOffset += uint8Chunk.byteLength;");
-    sb.append(    "}");
-    sb.append(    "if (!uint8Buffer.byteLength) throw 0;");
-    sb.append(  "}");
-    sb.append(  "catch(e) {");
-    sb.append(    "arrayBuffer = uint8Buffer = null;");
-    sb.append(  "}");
-    sb.append(  "return arrayBuffer;");
-    sb.append("}");
-    sb.append("\n");
-
-    sb.append("var _GM_deleteCacheFile = function(UUID) {");
-    sb.append(  "return " + jsBridgeName + ".deleteCacheFile(" + defaultSignature + ", UUID);");
-    sb.append("}");
-    sb.append("\n");
-
-    sb.append("var GM_cookie = {};");
-    sb.append("\n");
-
-    sb.append("GM_cookie.list = function(details, callback) {");
-    sb.append(  "if (typeof details === 'function') {callback = details; details = {};}");
-    sb.append(  "if (!details || (typeof details !== 'object')) details = {};");
-    sb.append(  "if (typeof callback !== 'function') return;");
-    sb.append(  "var url, cookies;");
-    sb.append(  "url = details.url ? details.url : details.domain ? ('https://' + details.domain) : unsafeWindow.location.href;");
-    sb.append(  "cookies = JSON.parse(");
-    sb.append(    jsBridgeName);
-    sb.append(    ".listCookies(");
-    sb.append(      defaultSignature);
-    sb.append(      ", url");
-    sb.append(    ")");
-    sb.append(  ");");
-    sb.append(  "if (details.name) {");
-    sb.append(    "cookies = cookies.filter(function(cookie){return (cookie.name === details.name);});"); // note: cookie names ARE case-sensitive
-    sb.append(  "}");
-    sb.append(  "if (details.decode) {");
-    sb.append(    "for (var i=0; i < cookies.length; i++) {");
-    sb.append(      "cookies[i].value = unsafeWindow.decodeURIComponent(cookies[i].value);");
-    sb.append(    "}");
-    sb.append(  "}");
-    sb.append(  "callback(cookies, null);");
-    sb.append("};");
-    sb.append("\n");
-
-    sb.append("GM_cookie.set = function(details, callback) {");
-    sb.append(  "var url, value, maxAge;");
-    sb.append(  "if (details && (typeof details === 'object') && details.name && details.value) {");
-    sb.append(    "url = details.url ? details.url : details.domain ? ('https://' + details.domain) : unsafeWindow.location.href;");
-    sb.append(    "value = details.encode ? unsafeWindow.encodeURIComponent(details.value) : details.value;");
-    sb.append(    "if (details.expirationDate && (typeof details.expirationDate === 'number')) {");
-    sb.append(      "maxAge = (new Date(details.expirationDate * 1000)) - (new Date());");
-    sb.append(      "maxAge = Math.floor(maxAge / 1000);");
-    sb.append(      "if (maxAge < 0) maxAge = 0;");
-    sb.append(    "}");
-    sb.append(    "else {");
-    sb.append(      "maxAge = -1;");
-    sb.append(    "}");
-    sb.append(    jsBridgeName);
-    sb.append(    ".setCookie(");
-    sb.append(      defaultSignature);
-    sb.append(      ", url, details.name, value, !!details.secure, !!details.httpOnly, maxAge");
-    sb.append(    ");");
-    sb.append(  "}");
-    sb.append(  "if (typeof callback === 'function') {");
-    sb.append(    "callback();");
-    sb.append(  "}");
-    sb.append("};");
-    sb.append("\n");
-
-    sb.append("GM_cookie.delete = function(details, callback) {");
-    sb.append(  "var url;");
-    sb.append(  "if (details && (typeof details === 'object') && details.name) {");
-    sb.append(    "url = details.url ? details.url : details.domain ? ('https://' + details.domain) : unsafeWindow.location.href;");
-    sb.append(    jsBridgeName);
-    sb.append(    ".deleteCookie(");
-    sb.append(      defaultSignature);
-    sb.append(      ", url, details.name");
-    sb.append(    ");");
-    sb.append(  "}");
-    sb.append(  "if (typeof callback === 'function') {");
-    sb.append(    "callback();");
-    sb.append(  "}");
-    sb.append("};");
-    sb.append("\n");
-
-    sb.append(GM_API_V4_POLYFILL);
-
-    return sb.toString();
+    String value;
+    value = StringUtils.replace(
+      GM_API_LEGACY_SIGNED_TEMPLATE,
+      "{{GLOBAL_JS_OBJECT}}",
+      GLOBAL_JS_OBJECT
+    );
+    value = StringUtils.replace(
+      value,
+      "{{jsBridgeName}}",
+      jsBridgeName
+    );
+    value = StringUtils.replace(
+      value,
+      "{{defaultSignature}}",
+      defaultSignature
+    );
+    value = StringUtils.replace(
+      value,
+      "{{callbackPrefix}}",
+      callbackPrefix
+    );
+    return value;
   }
 
   protected String getJsUserscript(Script script, String jsBeforeScript, String jsAfterScript) {
---
+++ android-studio-project/libs/webview-gm-lib/src/main/java/org/apache/commons/lang3/StringUtils.java
@@ -0,0 +1,140 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.lang3;
+
+public class StringUtils {
+
+
+    // https://bugs.openjdk.org/browse/JDK-8058779
+    // https://github.com/apache/commons-lang/blob/LANG_3_3_2/src/main/java/org/apache/commons/lang3/StringUtils.java#L4461-L4486
+
+    /**
+     * <p>Replaces all occurrences of a String within another String.</p>
+     *
+     * <p>A {@code null} reference passed to this method is a no-op.</p>
+     *
+     * <pre>
+     * StringUtils.replace(null, *, *)        = null
+     * StringUtils.replace("", *, *)          = ""
+     * StringUtils.replace("any", null, *)    = "any"
+     * StringUtils.replace("any", *, null)    = "any"
+     * StringUtils.replace("any", "", *)      = "any"
+     * StringUtils.replace("aba", "a", null)  = "aba"
+     * StringUtils.replace("aba", "a", "")    = "b"
+     * StringUtils.replace("aba", "a", "z")   = "zbz"
+     * </pre>
+     *
+     * @see #replace(String text, String searchString, String replacement, int max)
+     * @param text  text to search and replace in, may be null
+     * @param searchString  the String to search for, may be null
+     * @param replacement  the String to replace it with, may be null
+     * @return the text with any replacements processed,
+     *  {@code null} if null String input
+     */
+    public static String replace(final String text, final String searchString, final String replacement) {
+        return replace(text, searchString, replacement, -1);
+    }
+
+    // https://github.com/apache/commons-lang/blob/LANG_3_3_2/src/main/java/org/apache/commons/lang3/StringUtils.java#L4488-L4540
+
+    /**
+     * <p>Replaces a String with another String inside a larger String,
+     * for the first {@code max} values of the search String.</p>
+     *
+     * <p>A {@code null} reference passed to this method is a no-op.</p>
+     *
+     * <pre>
+     * StringUtils.replace(null, *, *, *)         = null
+     * StringUtils.replace("", *, *, *)           = ""
+     * StringUtils.replace("any", null, *, *)     = "any"
+     * StringUtils.replace("any", *, null, *)     = "any"
+     * StringUtils.replace("any", "", *, *)       = "any"
+     * StringUtils.replace("any", *, *, 0)        = "any"
+     * StringUtils.replace("abaa", "a", null, -1) = "abaa"
+     * StringUtils.replace("abaa", "a", "", -1)   = "b"
+     * StringUtils.replace("abaa", "a", "z", 0)   = "abaa"
+     * StringUtils.replace("abaa", "a", "z", 1)   = "zbaa"
+     * StringUtils.replace("abaa", "a", "z", 2)   = "zbza"
+     * StringUtils.replace("abaa", "a", "z", -1)  = "zbzz"
+     * </pre>
+     *
+     * @param text  text to search and replace in, may be null
+     * @param searchString  the String to search for, may be null
+     * @param replacement  the String to replace it with, may be null
+     * @param max  maximum number of values to replace, or {@code -1} if no maximum
+     * @return the text with any replacements processed,
+     *  {@code null} if null String input
+     */
+    public static String replace(final String text, final String searchString, final String replacement, int max) {
+        if (isEmpty(text) || isEmpty(searchString) || replacement == null || max == 0) {
+            return text;
+        }
+        int start = 0;
+        int end = text.indexOf(searchString, start);
+        if (end == INDEX_NOT_FOUND) {
+            return text;
+        }
+        final int replLength = searchString.length();
+        int increase = replacement.length() - replLength;
+        increase = increase < 0 ? 0 : increase;
+        increase *= max < 0 ? 16 : max > 64 ? 64 : max;
+        final StringBuilder buf = new StringBuilder(text.length() + increase);
+        while (end != INDEX_NOT_FOUND) {
+            buf.append(text.substring(start, end)).append(replacement);
+            start = end + replLength;
+            if (--max == 0) {
+                break;
+            }
+            end = text.indexOf(searchString, start);
+        }
+        buf.append(text.substring(start));
+        return buf.toString();
+    }
+
+    // https://github.com/apache/commons-lang/blob/LANG_3_3_2/src/main/java/org/apache/commons/lang3/StringUtils.java#L199-L220
+
+    /**
+     * <p>Checks if a CharSequence is empty ("") or null.</p>
+     *
+     * <pre>
+     * StringUtils.isEmpty(null)      = true
+     * StringUtils.isEmpty("")        = true
+     * StringUtils.isEmpty(" ")       = false
+     * StringUtils.isEmpty("bob")     = false
+     * StringUtils.isEmpty("  bob  ") = false
+     * </pre>
+     *
+     * <p>NOTE: This method changed in Lang version 2.0.
+     * It no longer trims the CharSequence.
+     * That functionality is available in isBlank().</p>
+     *
+     * @param cs  the CharSequence to check, may be null
+     * @return {@code true} if the CharSequence is empty or null
+     * @since 3.0 Changed signature from isEmpty(String) to isEmpty(CharSequence)
+     */
+    public static boolean isEmpty(final CharSequence cs) {
+        return cs == null || cs.length() == 0;
+    }
+
+    // https://github.com/apache/commons-lang/blob/LANG_3_3_2/src/main/java/org/apache/commons/lang3/StringUtils.java#L164-L168
+
+    /**
+     * Represents a failed index search.
+     * @since 2.1
+     */
+    public static final int INDEX_NOT_FOUND = -1;
+}
